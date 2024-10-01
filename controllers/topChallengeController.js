5b1c3eb42b260c4e9eefc5f71843e6ec197b2cd9
Camille Ang <S223646635@deakin.edu.au>
2024-10-01 22:56:05 +1000

    Updated with Trending MVC

---
+++ controllers/topChallengeController.js
@@ -0,0 +1,17 @@
+const { fetchTopChallenge } = require(`../models/topChallengeModel`);
+ 
+const getTopChallenge = async (req, res) => {
+  try {
+    const topChallenge = await fetchTopChallenge();
+ 
+    res.render('challenges', {
+      challenges: req.challenges,
+      topChallenge,
+    });
+  } catch (error) {
+    console.error('Error fetching top challenges:', error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+ 
+module.exports = { getTopChallenge };
